32dd1c42d563e0fcf698615aa9f629fed62b62ce
Ashley Williams <ashley666ashley@gmail.com>
2017-07-09 19:41:02 -0400

    feat(deploy): deploy from travis on merge to master

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd _book
+
+git init
+git config user.name "ashley williams"
+git config user.email "ashley666ashley@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/ashleygwilliams/rustbridge-www.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
