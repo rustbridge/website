234db92be74975eea3bec6d5aef84f3a9f78717a
Ashley Williams <ashley666ashley@gmail.com>
2017-09-14 10:28:53 -0400

    fix(build): build site in _site dir, deploy from there

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ set -o errexit -o nounset
 
 rev=$(git rev-parse --short HEAD)
 
+cd _site
+
 git init
 git config user.name "ashley williams"
 git config user.email "ashley666ashley@gmail.com"
@@ -16,4 +18,4 @@ touch .
 
 git add -A .
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+git push -fq upstream HEAD:gh-pages
